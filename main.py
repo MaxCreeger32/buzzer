fbd9e079994e6dedf9989476dfb20e2f5a0805bc
MaxCreeger32 <31531494+MaxCreeger32@users.noreply.github.com>
2021-08-31 10:45:04 +0200

    ajout gestion attente reponse

--- main.py
+++ main.py
@@ -1,64 +1,107 @@
-def on_button_pressed_a():
-    global id2
-    if id2 == "A":
-        id2 = "D"
-        basic.show_string(id2)
-    elif id2 == "B":
-        id2 = "A"
-        basic.show_string(id2)
-    elif id2 == "C":
-        id2 = "B"
-        basic.show_string(id2)
-    elif id2 == "D":
-        id2 = "C"
-        basic.show_string(id2)
-input.on_button_pressed(Button.A, on_button_pressed_a)
+"""
+
+démarrage
+
+"""
+config = False
+id2 = ""
+game.set_score(0)
+game.show_score()
+radio.set_group(1)
+id2 = "A"
+music.set_built_in_speaker_enabled(True)
+music.set_volume(255)
+attente_reponse = False
+
+def on_forever():
+    game.show_score()
+    pass
+basic.forever(on_forever)
+# appui sur le buzzer
+def on_pin_pressed_p0():
+    global attente_reponse, id2
+    if not (attente_reponse):
+        radio.send_value(id2, 1)
+        attente_reponse = True
+        music.start_melody(music.built_in_melody(Melodies.BA_DING), MelodyOptions.ONCE)
+        for i in range(6):
+            animationCarre()
+        pause(5000)
+        game.show_score()
+        basic.show_number(game.score())
+input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
+
+def animationCarre():
+    basic.show_leds("""
+        . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+    """)
+    pause(50)
+    basic.show_icon(IconNames.SMALL_SQUARE)
+    pause(50)
+    basic.show_icon(IconNames.SQUARE)
+    pause(50)
+# -----------DBEUT CONFIG DEVICE--------------
+# lance la config (identifiant du device)
 
 def on_button_pressed_ab():
     global id2, config
-    if config:
+    if not (config):
+        basic.show_string("Config ID")
         basic.show_string("A")
         id2 = "A"
         config = True
     else:
         config = False
+        game.show_score()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_a():
+    global id2
+    if config:
+        if id2 == "A":
+            id2 = "D"
+            basic.show_string(id2)
+        elif id2 == "B":
+            id2 = "A"
+            basic.show_string(id2)
+        elif id2 == "C":
+            id2 = "B"
+            basic.show_string(id2)
+        elif id2 == "D":
+            id2 = "C"
+            basic.show_string(id2)
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     global id2
-    if id2 == "A":
-        id2 = "B"
-        basic.show_string(id2)
-    elif id2 == " B":
-        id2 = "C"
-        basic.show_string(id2)
-    elif id2 == "C":
-        id2 = "D"
-        basic.show_string(id2)
-    elif id2 == "D":
-        id2 = "A"
-        basic.show_string(id2)
+    if config:
+        if id2 == "A":
+            id2 = "B"
+            basic.show_string(id2)
+        elif id2 == "B":
+            id2 = "C"
+            basic.show_string(id2)
+        elif id2 == "C":
+            id2 = "D"
+            basic.show_string(id2)
+        elif id2 == "D":
+            id2 = "A"
+            basic.show_string(id2)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+# -----------FIN CONFIG DEVICE----------------
+# à la réception on va savoir si c'est juste ou pas 
+
 def on_received_value(name, value):
-    if name == "A" and value == 1:
-        basic.show_icon(IconNames.YES)
-    elif name == "B" and value == 0:
-        basic.show_icon(IconNames.NO)
-    else:
-        basic.show_icon(IconNames.ASLEEP)
+    if (attente_reponse):
+        if name == id2 and value == 1:
+            game.add_score(1)
+            music.start_melody(music.built_in_melody(Melodies.POWER_UP), 120)
+        elif name == id2 and value == 0:
+            music.start_melody(music.built_in_melody(Melodies.POWER_DOWN), 120)
+        game.show_score()
 radio.on_received_value(on_received_value)
-
-config = False
-id2 = ""
-basic.show_icon(IconNames.HAPPY)
-radio.set_group(1)
-id2 = "A"
-
-def on_forever():
-    pass
-basic.forever(on_forever)
-
-def on_pin_pressed_p0():
-    radio.send_value(id2, 1)
-input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
--- main.ts
+++ main.ts
@@ -1,40 +1,40 @@
-/**
- * démarrage
- */
+/** démarrage */
 let config = false
-let id = ""
-
+let id2 = ""
 game.setScore(0)
 game.showScore()
 radio.setGroup(1)
-id = "A"
+id2 = "A"
 music.setBuiltInSpeakerEnabled(true)
 music.setVolume(255)
-
-basic.forever(function () {
+let attente_reponse = false
+basic.forever(function on_forever() {
+    game.showScore()
     
 })
-
-input.onPinPressed(TouchPin.P0, function () {
-    radio.sendValue(id, 1)
-    music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
-    for (let i=0 ;i<=5; i++)
-    {
-        animationCarre()
+//  appui sur le buzzer
+input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
+    
+    if (!attente_reponse) {
+        radio.sendValue(id2, 1)
+        attente_reponse = true
+        music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
+        for (let i = 0; i < 6; i++) {
+            animationCarre()
+        }
+        pause(5000)
+        game.showScore()
+        basic.showNumber(game.score())
     }
-    pause(5000)
-    game.showScore()
-    basic.showNumber(game.score())
+    
 })
-
-function animationCarre()
-{
+function animationCarre() {
     basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . # . .
-    . . . . .
-    . . . . .
+        . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
     `)
     pause(50)
     basic.showIcon(IconNames.SmallSquare)
@@ -43,67 +43,73 @@ function animationCarre()
     pause(50)
 }
 
-
-//-----------DBEUT CONFIG DEVICE--------------
-// lance la config (identifiant du device)
-input.onButtonPressed(Button.AB, function () {
-    if (!(config)) {
-        basic.showString("Config ID")  
+//  -----------DBEUT CONFIG DEVICE--------------
+//  lance la config (identifiant du device)
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
+    if (!config) {
+        basic.showString("Config ID")
         basic.showString("A")
-        id = "A"
+        id2 = "A"
         config = true
     } else {
         config = false
         game.showScore()
     }
+    
 })
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     if (config) {
-        if (id == "A") {
-            id = "D"
-            basic.showString(id)
-        } else if (id == "B") {
-            id = "A"
-            basic.showString(id)
-        } else if (id == "C") {
-            id = "B"
-            basic.showString(id)
-        } else if (id == "D") {
-            id = "C"
-            basic.showString(id)
+        if (id2 == "A") {
+            id2 = "D"
+            basic.showString(id2)
+        } else if (id2 == "B") {
+            id2 = "A"
+            basic.showString(id2)
+        } else if (id2 == "C") {
+            id2 = "B"
+            basic.showString(id2)
+        } else if (id2 == "D") {
+            id2 = "C"
+            basic.showString(id2)
         }
+        
     }
+    
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
     if (config) {
-        if (id == "A") {
-            id = "B"
-            basic.showString(id)
-        } else if (id == "B") {
-            id = "C"
-            basic.showString(id)
-        } else if (id == "C") {
-            id = "D"
-            basic.showString(id)
-        } else if (id == "D") {
-            id = "A"
-            basic.showString(id)
+        if (id2 == "A") {
+            id2 = "B"
+            basic.showString(id2)
+        } else if (id2 == "B") {
+            id2 = "C"
+            basic.showString(id2)
+        } else if (id2 == "C") {
+            id2 = "D"
+            basic.showString(id2)
+        } else if (id2 == "D") {
+            id2 = "A"
+            basic.showString(id2)
         }
+        
     }
+    
 })
-//-----------FIN CONFIG DEVICE----------------
-
-// à la réception on va savoir si c'est juste ou pas 
-radio.onReceivedValue(function (name, value) {
-    if (name == id && value == 1) 
-    {
-        game.addScore(1)
-        music.startMelody(music.builtInMelody(Melodies.PowerUp), 120)     
+//  -----------FIN CONFIG DEVICE----------------
+//  à la réception on va savoir si c'est juste ou pas 
+radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    if (attente_reponse) {
+        if (name == id2 && value == 1) {
+            game.addScore(1)
+            music.startMelody(music.builtInMelody(Melodies.PowerUp), 120)
+        } else if (name == id2 && value == 0) {
+            music.startMelody(music.builtInMelody(Melodies.PowerDown), 120)
+        }
+        
+        game.showScore()
     }
-    else if (name == id && value == 0)
-    {
-        music.startMelody(music.builtInMelody(Melodies.PowerDown), 120)     
-    }
-    game.showScore()
+    
 })
-
